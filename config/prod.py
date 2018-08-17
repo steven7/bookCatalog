895d4179e3de9a56517d1203a35318ab90266786
steven7 <steven.kanceruk@gmail.com>
2018-08-17 02:52:46 -0400

    first commit

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,7 @@
+import os
+
 DEBUG = False
 SECRET_KEY='superuser'
-SQLALCHEMY_DATABASE_URI='postgresql://postgres:superuser@localhost/catalog_db'
+# SQLALCHEMY_DATABASE_URI='postgresql://postgres:superuser@localhost/catalog_db' #
+SQLALCHEMY_DATABASE_URI=os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS=False
